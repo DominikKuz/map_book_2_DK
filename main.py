eac902f8786e82178c5c0d55c70ee191fc0dfbf1
DominikKuz <167193705+DominikKuz@users.noreply.github.com>
2024-05-28 20:03:26 +0200

    wip: prepare a for loop

--- main.py
+++ main.py
@@ -1,17 +1,9 @@
-name="Dominik"
-surname="Kuźnik"
-post=4
+users: list[dict] = [
+    {'name': 'Dominik', 'surname': 'Kuźnik', 'post': 1},
+    {'name': 'Kacper', 'surname': 'Macioch', 'post': 2},
+    {'name': 'Michał', 'surname': 'Krzywiński', 'post': 3},
+    {'name': 'Tymon', 'surname': 'Leszczyc', 'post': 2},
+]
 
-print(f"Twój znajomy {name} {surname} opublikował {post} postów")
-
-name_2="Jakub"
-surname_2="Kołodziejczyk"
-post_2=1
-
-print(f"Twój znajomy {name_2} {surname_2} opublikował {post_2} postów")
-
-name_3="Jakub"
-surname_3="Łojek"
-post_3=2
-
-print(f"Twój znajomy {name_3} {surname_3} opublikował {post_3} postów")
+for user in users:
+    print(f"Twój znajomy {user['name']} opublikował: {user['post']} postów")
