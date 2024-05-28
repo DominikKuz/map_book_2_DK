3cb22ab9dae550f75c7002a4fafd6ef0c4ba1693
DominikKuz <167193705+DominikKuz@users.noreply.github.com>
2024-05-28 22:27:08 +0200

    wip: add map functions

--- models/data.py
+++ models/data.py
@@ -1,8 +1,10 @@
 users: list[dict[str, str]] = [
-    {'name': 'Dominik', 'surname': 'Kuźnik', 'post': 1},
-    {'name': 'Kacper', 'surname': 'Macioch', 'post': 2},
-    {'name': 'Michał', 'surname': 'Krzywiński', 'post': 3},
-    {'name': 'Tymon', 'surname': 'Leszczyc', 'post': 2},
-    {'name': 'Michał', 'surname': 'Lębryk', 'post': 2},
+    {'name': 'Dominik', 'surname': 'Kuźnik', 'post': 1, 'location':'Konin'},
+    {'name': 'Kacper', 'surname': 'Macioch', 'post': 2, 'location':'Ząbki'},
+    {'name': 'Michał', 'surname': 'Krzywiński', 'post': 3, 'location':'Brodnica'},
+    {'name': 'Tymon', 'surname': 'Leszczyc', 'post': 2, 'location':'Toruń'},
+    {'name': 'Michał', 'surname': 'Lębryk', 'post': 2, 'location':'Lublin'},
 ]
+
+
 
