de64c36341078ddd8e114b6ab3df7c046c768708
DominikKuz <167193705+DominikKuz@users.noreply.github.com>
2024-05-28 20:33:32 +0200

    wip: add a menu

--- main.py
+++ main.py
@@ -3,7 +3,22 @@ users: list[dict] = [
     {'name': 'Kacper', 'surname': 'Macioch', 'post': 2},
     {'name': 'Michał', 'surname': 'Krzywiński', 'post': 3},
     {'name': 'Tymon', 'surname': 'Leszczyc', 'post': 2},
-]
 
-for user in users:
-    print(f"Twój znajomy {user['name']} opublikował: {user['post']} postów")
+]
+def show_users(user_list: list[dict]) -> None:
+    for user in user_list:
+        print(f"Twój znajomy {user['name']} opublikował: {user['post']} postów")
+
+if __name__ == '__main__':
+    print:("Witaj użytkowniku")
+    while True:
+
+        print("Menu:")
+        print("1. Wyświetl co u znajomych")
+        menu_option:str=input("Dokonaj wyboru")
+        if menu_option == "0":
+            print("Program kończy pracę")
+            break
+        if menu_option == "1":
+            show_users(users)
+
