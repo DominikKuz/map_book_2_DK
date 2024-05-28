9667811c7832b43c33f156bf7c43484424758928
DominikKuz <167193705+DominikKuz@users.noreply.github.com>
2024-05-28 22:17:55 +0200

    wip: add next functions

--- models/data.py
+++ models/data.py
@@ -1,7 +1,8 @@
-users: list[dict] = [
+users: list[dict[str, str]] = [
     {'name': 'Dominik', 'surname': 'Kuźnik', 'post': 1},
     {'name': 'Kacper', 'surname': 'Macioch', 'post': 2},
     {'name': 'Michał', 'surname': 'Krzywiński', 'post': 3},
     {'name': 'Tymon', 'surname': 'Leszczyc', 'post': 2},
-
+    {'name': 'Michał', 'surname': 'Lębryk', 'post': 2},
 ]
+
