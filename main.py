9905283e7cc69805c213cf247867d9d75d77e4da
DominikKuz <167193705+DominikKuz@users.noreply.github.com>
2024-05-28 19:52:46 +0200

    wip: Add user printing functionality

---
+++ main.py
@@ -0,0 +1,17 @@
+name="Dominik"
+surname="Kuźnik"
+post=4
+
+print(f"Twój znajomy {name} {surname} opublikował {post} postów")
+
+name_2="Jakub"
+surname_2="Kołodziejczyk"
+post_2=1
+
+print(f"Twój znajomy {name_2} {surname_2} opublikował {post_2} postów")
+
+name_3="Jakub"
+surname_3="Łojek"
+post_3=2
+
+print(f"Twój znajomy {name_3} {surname_3} opublikował {post_3} postów")
