bda8c6d9a2a98cdc9f8d685deacaa17578fc32a0
DominikKuz <167193705+DominikKuz@users.noreply.github.com>
2024-05-28 22:02:26 +0200

    wip: add function add users

--- utils/crud.py
+++ utils/crud.py
@@ -1,3 +1,9 @@
-def show_users(user_list: list[dict]) -> None:
+def read(user_list: list[dict]) -> None:
     for user in user_list:
         print(f"Twój znajomy {user['name']} opublikował: {user['post']} postów")
+
+
+def add_user(user: list) -> None:
+    user_name = input ('Podaj imię')
+    user_surname = input('Podaj nazwisko')
+    user_post = input('Podaj liczbę postów')
